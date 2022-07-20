b392d0afe2797412f2fcc3430d4fae41e319adcd
Marat Kazgozhin <Marat.Kazgozhin@btsdigital.kz>
2022-07-20 18:26:18 +0600

    Init commit

---
+++ src/main/java/kz/halykacademy/testappp/TestapppApplication.java
@@ -0,0 +1,13 @@
+package kz.halykacademy.testappp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestapppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestapppApplication.class, args);
+	}
+
+}
