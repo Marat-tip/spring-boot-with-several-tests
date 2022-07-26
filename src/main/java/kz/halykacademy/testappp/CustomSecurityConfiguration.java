5f25b2fc2b5afc03d4a3cc16443af8a033e49b0c
Marat Kazgozhin <marat.kazgozhin@finteh.llc>
2022-07-26 14:19:54 +0600

    Basic auth with authorities check

--- src/main/java/kz/halykacademy/testappp/CustomSecurityConfiguration.java
+++ src/main/java/kz/halykacademy/testappp/CustomSecurityConfiguration.java
@@ -2,8 +2,12 @@ package kz.halykacademy.testappp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +16,13 @@ public class CustomSecurityConfiguration {
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
         return new UserDetailsImpl(userRepository);
+    }
+
+    @Bean
+    public SecurityFilterChain customFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/v1/**").authenticated()
+                .antMatchers( "/v1/**").hasAuthority("ADMIN")
+                .and().httpBasic(Customizer.withDefaults()).build();
     }
 }
--- src/main/java/kz/halykacademy/testappp/UserController.java
+++ src/main/java/kz/halykacademy/testappp/UserController.java
@@ -5,17 +5,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/v1/users")
 public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<Void> createUser(CreateUserRequest userRequest) {
+    public ResponseEntity<Void> createUser(@RequestBody CreateUserRequest userRequest) {
         userService.createUser(userRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
